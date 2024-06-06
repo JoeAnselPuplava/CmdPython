e52e237f6d1a701e2597db777a2c73bb7e653ee8
Joe-Ansel Puplava <Joe-Ansel.Puplava@tufts.edu>
2024-06-06 13:15:11 -0400

    Made the program able to send queries to a database

--- dpsql_tool/build/lib/dpsql/__main__.py
+++ dpsql_tool/build/lib/dpsql/__main__.py
@@ -4,9 +4,15 @@ try:
     import psycopg2
     import psycopg2.extras
     import pandas as pd
+    import json
+    from IPython.display import display
     print("libraries imported")
 except ImportError as e:
     print(f"Error importing libraries: {e}")
+
+global connection, cursor
+
+CONFIG_FILE = "db_config.json"
 
 #Function made in privateDS
 def postgres_DB(dbname, user, host, password):
@@ -17,6 +23,7 @@ def postgres_DB(dbname, user, host, password):
             # Create a cursor
             cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
             print("Connected to database, cursor defined")
+            save_config(dbname, user, host, password)
             return conn, cur
         except psycopg2.Error as e:
             print(f"Error creating cursor: {e}")
@@ -28,34 +35,11 @@ def postgres_DB(dbname, user, host, password):
         return None, None
 
 def query(args):
-    print(f"The query you entered is: {args.query}")
+    config = load_config()
+    _, cursor = postgres_DB(config["dbname"], config["user"], config["hostname"], config["password"])
 
-
-def showTables(cursor):
-    
-    # Get table names from the information schema
-    cursor.execute("SELECT TABLE_NAME \
-                    FROM INFORMATION_SCHEMA.TABLES \
-                   WHERE TABLE_TYPE = 'BASE TABLE' \
-                     AND TABLE_SCHEMA='public' ")
-    
-    # Fetch column names from the cursor description
-    column_names = [desc[0] for desc in cursor.description]
-    for x in column_names:
-        print(x)
-
-    # Create a DataFrame from the fetched data
-    df = pd.DataFrame(cursor.fetchall(), columns=column_names)
-    display(df) 
-
-def customQuery(cursor):
-    # Ask user for a query
-    print("Enter your query (type stop to end):")
-    query = input()
-    if (query == "stop"):
-        return query
     # Execute query
-    cursor.execute(query)
+    cursor.execute(args.query)
 
     # Fetch column names from the cursor description
     column_names = [desc[0] for desc in cursor.description]
@@ -63,55 +47,35 @@ def customQuery(cursor):
     # Create a DataFrame from the fetched data
     df = pd.DataFrame(cursor.fetchall(), columns=column_names)
     # return df
-    display(df)  
-    print("Invalid Query")
-        
-    return query
+    display(df)
+
+# Saves the database details so that the program can 
+# connect to the database without prompting user for all the information
+def save_config(dbname, user, host, password):
+    config = {
+        "hostname": host,
+        "dbname": dbname,
+        "user": user,
+        "password": password,
+    }
+    with open(CONFIG_FILE, "w") as file:
+        json.dump(config, file)
+
+# Loads the previously saved information about the database
+def load_config():
+    try:
+        with open(CONFIG_FILE, "r") as file:
+            config = json.load(file)
+        return config
+    except FileNotFoundError:
+        return None
 
 def connDB(args):
-    print(f"{args.name}")
-    print(f"{args.username}")
-    print(f"{args.hostname}")
-    print(f"{args.password}")
-    # postgres_DB({args.name}, {args.username}, {args.hostname}, {args.password})
-    postgres_DB(f"{args.name}", f"{args.username}", f"{args.hostname}", f"{args.password}")
-
-def userInput():
-    # "university"
-    print("Enter database name: ")
-    # DBname = input()
-    DBname = "university"
-
-    # "postgres"
-    print("Enter userName: ")
-    # userName = input()
-    userName = "postgres"
-
-    # "localhost"
-    print("Enter host name: ")
-    # hostName = input()
-    hostName = "localhost"
-
-    # ""
-    print("Enter password: ")
-    # password = input()
-    password = ""
-    
-
-    print("Connecting to PostGresDB")
-    conn, curs = postgres_DB("university", "postgres", "localhost", "")
-    # conn, curs = postgres_DB(DBname, userName, hostName, password)
-    showTables(curs)
-    query = ""
-    while (query != "stop"):
-        try:
-            query = customQuery(curs)
-        except:
-            print("Invalid Query")
-            conn, curs = postgres_DB(DBname, userName, hostName, password)
-
-    print("Good Bye")
-
+    print(f"DBName: {args.dbname}")
+    print(f"Username: {args.user}")
+    print(f"Hostname: {args.hostname}")
+    print(f"Password: {args.password}")
+    postgres_DB(f"{args.dbname}", f"{args.user}", f"{args.hostname}", f"{args.password}")
 
 def main():
     parser = argparse.ArgumentParser(description="A command line tool to query PostGres databases")
@@ -125,11 +89,18 @@ def main():
     parser_query.set_defaults(func=query)
 
     # Connect command
+    # Use defaults from config file if available
+    config = load_config()
+    def_dbname   = config["dbname"]   if config else "my_database"
+    def_user     = config["user"]     if config else "my_user"
+    def_hostname = config["hostname"] if config else "localhost"
+    def_password = config["password"] if config else "my_password"
+
     parser_query = subparsers.add_parser('connect',  help="Enter a query to search the database")
-    parser_query.add_argument('name',     type=str, help="The name of the postgres database")
-    parser_query.add_argument('username', type=str, help="The username to connect to postgres database")
-    parser_query.add_argument('hostname', type=str, help="The hostname to connect to postgres database")
-    parser_query.add_argument('password', type=str, help="The password to connect to postgres database")
+    parser_query.add_argument('dbname',   nargs='?', type=str, default=def_dbname,   help="The name of the postgres database")
+    parser_query.add_argument('user',     nargs='?', type=str, default=def_user,     help="The username to connect to postgres database")
+    parser_query.add_argument('hostname', nargs='?', type=str, default=def_hostname, help="The hostname to connect to postgres database")
+    parser_query.add_argument('password', nargs='?', type=str, default=def_password, help="The password to connect to postgres database")
     parser_query.set_defaults(func=connDB)
     
     # Parse arguments and call appropriate function
